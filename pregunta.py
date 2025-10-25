e6ea56ff203b375246032e72be32f9555033447a
a16b07 <josedaniel16sc@gmail.com>
2025-10-25 12:07:55 -0600

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -1,14 +1,14 @@
-from random import random
+import random
 
 class pregunta:
 
-    def __init__(self):
-        self.texto = ""
-        self.correcta = ""
-        self.incorrecta = [""]
-        self.valor  = 0
+    # def __init__(self):
+    #     self.texto = ""
+    #     self.correcta = ""
+    #     self.incorrecta = [""]
+    #     self.valor  = 0
 
-    def _init__(self, text, correct, incorrect, value):
+    def __init__(self, text, correct, incorrect, value):
         self.texto = text
         self.correcto = correct
         self.incorrecto = incorrect
@@ -16,12 +16,12 @@ class pregunta:
 
     def display(self):
         print(self.texto, "\n")
-        respuestas = [self.correcta]
-        for i in self.incorrecta:
-            respuestas.append(self.incorrecta[i])
-        random. shuffle(respuestas)
+        respuestas = [self.correcto]
+        for i in self.incorrecto:
+            respuestas.append(i)
+        random.shuffle(respuestas)
         for i in respuestas:
-            if self.correcta == respuestas[i]:
+            if self.correcto == i:
                 icorrecta = i
         print(respuestas)
         
