637f330089ce3e55ca1cc6e4751dfca775ac8225
CrisH2O <cristhianhuezo22@gmail.com>
2025-10-25 11:29:50 -0600

    Create pregunta.py

---
+++ pregunta.py
@@ -0,0 +1,26 @@
+from random import random
+
+class pregunta:
+
+    def __init__(self):
+        self.texto = ""
+        self.correcta = ""
+        self.incorrecta = [""]
+        self.valor  = 0
+
+    def _init__(self, text, correct, incorrect, value):
+        self.texto = text
+        self.correcto = correct
+        self.incorrecto = incorrect
+        self.valor = value
+
+    def display(self):
+        print(self.texto, "\n")
+        respuestas = [self.correcta, self.incorrecta]
+        random. shuffle(respuestas)
+        for i in respuestas:
+            if self.correcta == respuestas[i]:
+                icorrecta = i
+        print(respuestas)
+        
+        
